50547a8c32fc95691cb8fdc2bd505086b2c19468
Daria Havlovych <daria.havlovych@gmail.com>
2024-08-31 15:34:10 +0300

    add task01

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,22 @@ import {
   ForbesList,
 } from 'components';
 
+import article from 'data/article.json';
+
 export const App = () => {
   return (
     <Section>
       <Container>
         <Heading title="Task 1 Blog Card" bottom />
-        <BlogCard />
+        <BlogCard
+          name={article.name}
+          avatar={article.avatar}
+          description={article.description}
+          postedAt={article.postedAt}
+          poster={article.poster}
+          tag={article.tag}
+          title={article.title}
+        />
 
         <Heading title="Task 2 Statistics" top bottom />
         <Statistics title="Main Statistics" />
