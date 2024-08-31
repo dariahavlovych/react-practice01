1df096430bc9cb6f1af5be90fcc3ecc172baef2b
Yaroslav_Orlov <sanenss@gmail.com>
2024-08-31 16:19:26 +0300

    task-2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,29 @@
-// import { FaRegThumbsUp } from 'react-icons/fa';
-// import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
-// import { GiTreeDoor } from 'react-icons/gi';
-// import { StatisticsItem } from '../StatisticsItem/StatisticsItem';
-// import style from './Statistics.module.css';
+import { FaRegThumbsUp } from 'react-icons/fa';
+import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
+import { GiTreeDoor } from 'react-icons/gi';
+import { StatisticsItem } from '../StatisticsItem/StatisticsItem';
+import style from './Statistics.module.css';
 
-export const Statistics = () => {
-  return <h2>Statistics</h2>;
+export const Statistics = ({ title, stats }) => {
+  const iconsArray = [
+    <FaRegThumbsUp key="1" />,
+    <MdPeople key="2" />,
+    <MdOutlineProductionQuantityLimits key="3" />,
+    <GiTreeDoor key="4" />,
+  ];
+  return (
+    <>
+      {title && <h2 className={style.title}>{title}</h2>}
+      <ul className={style.list}>
+        {stats.map((stat, index) => (
+          <StatisticsItem
+            key={stat.id}
+            title={stat.title}
+            total={stat.total}
+            icon={iconsArray[index]}
+          />
+        ))}
+      </ul>
+    </>
+  );
 };
